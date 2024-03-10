068302df05077c5684aeebebe276f2680a1f8e12
tinywind <tinywind0@gmail.com>
2024-03-11 02:38:29 +0700

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,67 @@
+module.exports = {
+  root: true,
+  plugins: ['node', 'prettier', '@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'prettier',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/electron',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    // 'parser': 'espree',
+    allowImportExportEverywhere: true,
+    sourceType: 'module',
+    ecmaVersion: 'latest',
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  globals: {
+    JSX: true,
+  },
+  settings: {
+    'eslint.validate': ['javascript', 'javascriptreact', 'typescript', 'typescriptreact', 'vue', 'markdown'],
+    'editor.codeActionsOnSave': {
+      'source.fixAll.eslint': true,
+    },
+    'import/resolver': {
+      node: {
+        paths: [''],
+      },
+    },
+  },
+  ignorePatterns: ['node_modules/'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+        singleQuote: true,
+        jsxSingleQuote: true,
+        bracketSameLine: true,
+        proseWrap: 'preserve',
+        quoteProps: 'as-needed',
+        semi: true,
+        tabWidth: 2,
+        trailingComma: 'all',
+        useTabs: false,
+        printWidth: 200,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        vueIndentScriptAndStyle: false,
+      },
+      { usePrettierrc: true },
+    ],
+    'no-param-reassign': 0,
+    'no-shadow': 0,
+    'no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+  },
+};
---
+++ README.md
@@ -0,0 +1,4 @@
+# references
+
+- https://www.electronjs.org/docs/latest/tutorial/quick-start
+- https://www.electronforge.io/
